adc39e62d8f3f25f7dc628bf4dc7899b498e9cb4
chuang268 <chuang268@163.com>
2024-10-18 01:27:26 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -11,14 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.2.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 替换golang版本为1.22
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-# ttyd免登陆
-sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # design修改proxy链接
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
